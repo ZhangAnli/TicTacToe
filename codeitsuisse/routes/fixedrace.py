20ca32e44cf52141c561e389eba172a23835ac1a
ZhangAnli <zhanganli1998@hotmail.com>
2021-09-26 01:05:56 +0800

    Debug

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from flask import request, jsonify
 from codeitsuisse import app
@@ -7,7 +8,11 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def evaluateFixedRace():
-
-        data = request.get_data(as_text=True)
-        logging.info("My result :{}".format(data))
-        return data
+    data = request.get_data(as_text=True)
+    logging.info("My result :{}".format(data))
+    str = data.split(',')
+    random.shuffle(str)
+    result = ''
+    for i in str:
+        result += (i + ',')
+    return result[:-1]
