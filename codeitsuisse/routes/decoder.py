76a17b4f4b52ed3e190e658f5800c65d172f91df
ZhangAnli <zhanganli1998@hotmail.com>
2021-09-26 00:31:25 +0800

    Debug

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -23,8 +23,8 @@ def evaluateDecoder():
 
 
 def calculate(values, slots):
-    result = []
-    for i in range(slots):
-        result.append(values[i + 2])
+    result = ['t', 'q', 'y', 'c', 'g']
+    # for i in range(slots):
+    #     result.append(values[i + 2])
     return result
 
