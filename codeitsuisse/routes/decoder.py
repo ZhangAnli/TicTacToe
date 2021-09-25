32fdb154a3cfa2bbc71307d6597fa20b8c69c6cd
ZhangAnli <zhanganli1998@hotmail.com>
2021-09-25 19:55:24 +0800

    Fix bugs

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -6,24 +6,25 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
+
 @app.route('/decoder', methods=['GET', 'POST'])
 def evaluateDecoder():
+    data = request.get_json()
 
-        data = request.get_json()
+    possible_values = data["possible_values"]
+    num_slots = data["num_slots"]
+    history = data["history"]
 
-        possible_values = data["possible_values"]
-        num_slots = data["num_slots"]
-        history = data["history"]
+    result = [{
+        "answer": calculate(possible_values, num_slots, history)
+    }]
 
-        result = [{
-                "answer": calculate(possible_values, num_slots, history)
-        }]
+    logging.info("My result :{}".format(data))
+    return jsonify(result)
 
-        logging.info("My result :{}".format(data))
-        return jsonify(result)
 
 def calculate(values, slots, history):
-        result = []
-        for i in range(slots):
-                result.append(random.choice(values))
-        return result
+    result = []
+    for i in range(slots):
+        result.append(random.choice(values))
+    return result
